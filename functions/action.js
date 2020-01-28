ac2b34c4b7c3257fd36e18bc1a91d849ca12384e
Sohil Pandya <sohilpandya@me.com>
2020-01-28 12:44:08 +0000

    updated action lambda with better error handling ish

--- functions/action.js
+++ functions/action.js
@@ -1,41 +1,50 @@
 const axios = require("axios");
 
 exports.handler = function(event, context, callback) {
-  const id = event.queryStringParameters.associatedObjectId;
-  const uri = `https://api.hubapi.com/deals/v1/deal/${id}?hapikey=${process.env.HAPI_KEY}`;
+    const id = event.queryStringParameters.associatedObjectId;
+    const uri = `https://api.hubapi.com/deals/v1/deal/${id}?hapikey=${process.env.HAPI_KEY}`;
 
-  console.log(`youre in lambda /actions, ${uri}`);
+    console.log(`youre in lambda /actions, ${uri}`);
 
-  axios({
-    url: uri,
-    method: "put",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    data: {
-      properties: [
-        {
-          name: "dealstage",
-          value: "838999"
-        }
-      ]
-    }
-  })
-    .then(res => {
-      console.log(res);
-      res.statusCode === 200
-        ? callback(null, {
-            statusCode: 200,
-            body: JSON.stringify({
-              message: "Successful GET request"
-            })
-          })
-        : callback(null, {
-            statusCode: 400,
-            body: JSON.stringify({
-              message: "FAILURE"
-            })
-          });
-    })
-    .catch(err => console.log(err));
+    axios({
+            url: uri,
+            method: "put",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: {
+                properties: [{
+                    name: "dealstage",
+                    value: "838999"
+                }]
+            }
+        })
+        .then(res => {
+            if (res.status === 200) {
+
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        message: "Successful GET request"
+                    })
+                })
+            } else {
+                console.log(res)
+                callback(null, {
+                    statusCode: 400,
+                    body: JSON.stringify({
+                        message: "NOT A 200 STATUS"
+                    })
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            callback(null, {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: "CATCH ERRROR"
+                })
+            });
+        });
 };
