65313803481c1f101651d7c9823b21e582969c32
NickDrew <nicholasadrew@hotmail.com>
2020-01-28 14:42:57 +0000

    a

--- functions/hubspot.js
+++ functions/hubspot.js
@@ -74,14 +74,16 @@ exports.handler = function(event, context, callback) {
             "https://bat-staging.trussle.com/application/87c6b07f-97e8-44a4-bf40-19c5e28e7e84",
           label: "Submission"
         },
-        primaryAction: {
-          type: "IFRAME",
-          width: 890,
-          height: 748,
-          uri:
-            "https://bat-staging.trussle.com/edit/87c6b07f-97e8-44a4-bf40-19c5e28e7e84",
-          label: "Edit Customer Data"
-        }
+        secondaryActions: [
+          {
+            type: "IFRAME",
+            width: 890,
+            height: 748,
+            uri:
+              "https://bat-staging.trussle.com/edit/87c6b07f-97e8-44a4-bf40-19c5e28e7e84",
+            label: "Edit Customer Data"
+          }
+        ]
       })
     });
   });
