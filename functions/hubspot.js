bd31cbcdeaca0871e7f6b39479f7908a2a9a837d
Sohil Pandya <sohilpandya@me.com>
2020-01-28 10:31:30 +0000

    hello wrold

--- functions/hubspot.js
+++ functions/hubspot.js
@@ -1,14 +1,6 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
         statusCode: 200,
-        body: {
-            "results": {
-                "applicationId": 211393,
-                "title": "Testing Hubspot Sandbox App",
-                "associatedHubSpotObjectTypes": [
-                    "DEAL"
-                ],
-            },
-        }
+        body: "hello world!"
     });
 }
