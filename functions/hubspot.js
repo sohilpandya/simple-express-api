5dc9b6346c813cda3f4442054f68d633fb09ebc2
Sohil Pandya <sohilpandya@me.com>
2020-01-28 10:33:35 +0000

    new body

--- functions/hubspot.js
+++ functions/hubspot.js
@@ -1,6 +1,16 @@
 exports.handler = function(event, context, callback) {
+
+    console.log('youre in lambda ')
     callback(null, {
         statusCode: 200,
-        body: "hello world!"
+        body: JSON.stringify({
+            "results": {
+                "applicationId": 211393,
+                "title": "Testing Hubspot Sandbox App",
+                "associatedHubSpotObjectTypes": [
+                    "DEAL"
+                ],
+            },
+        })
     });
 }
