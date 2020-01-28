e631c2eadbcd3d5719c4c8d542eaae3e48864bbd
Sohil Pandya <sohilpandya@me.com>
2020-01-28 14:26:13 +0000

    return in then

--- functions/hubspot.js
+++ functions/hubspot.js
@@ -10,74 +10,75 @@ exports.handler = function(event, context, callback) {
     const getPipelineStages = axios.get(pipelineURI).then((res) => {
         const currentPipeline = res.results.filter((i) => i.label === pipeline)
         return _.orderBy(currentPipeline[0].stages, 'displayOrder');
+    }).then((currentStages) => {
+        console.log(currentStages)
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+                results: [{
+                    objectId: 245,
+                    title: `${event.queryStringParameters.dealname}`,
+                    link: "http://example.com/1",
+                    created: "2016-09-15",
+                    priority: "HIGH",
+                    project: "API",
+                    reported_by: "msmith@hubspot.com",
+                    description: "Customer reported that the APIs are just running too fast. This is causing a problem in that they're so happy.",
+                    reporter_type: "Account Manager",
+                    status: "In Progress",
+                    ticket_type: "Bug",
+                    updated: "2016-09-28",
+                    actions: [{
+                            type: "CONFIRMATION_ACTION_HOOK",
+                            httpMethod: "GET",
+                            uri: "https://hopeful-golick-30e6f5.netlify.com/.netlify/functions/action",
+                            label: "Example action",
+                            associatedObjectProperties: [
+                                "dealname",
+                                "dealstage",
+                                "hs_object_id"
+                            ],
+                            confirmationMessage: "Are you sure you want to run example action?",
+                            confirmButtonText: "Yes",
+                            cancelButtonText: "No"
+                        },
+                        {
+                            type: "ACTION_HOOK",
+                            httpMethod: "GET",
+                            uri: "https://hopeful-golick-30e6f5.netlify.com/.netlify/functions/action",
+                            label: "no prompt action",
+                            associatedObjectProperties: [
+                                "dealname",
+                                "dealstage",
+                                "hs_object_id"
+                            ]
+                        },
+                        {
+                            type: "IFRAME",
+                            width: 890,
+                            height: 748,
+                            uri: "https://example.com/iframe-contents",
+                            label: "Test iFrame",
+                            associatedObjectProperties: []
+                        }
+                    ]
+                }],
+                submissionAction: {
+                    type: "IFRAME",
+                    width: 890,
+                    height: 748,
+                    uri: "https://bat-staging.trussle.com/application/87c6b07f-97e8-44a4-bf40-19c5e28e7e84",
+                    label: "Submission"
+                },
+                editDetailsAction: {
+                    type: "IFRAME",
+                    width: 890,
+                    height: 748,
+                    uri: "https://bat-staging.trussle.com/edit/87c6b07f-97e8-44a4-bf40-19c5e28e7e84",
+                    label: "Edit Customer Data"
+                }
+            })
+        });
     })
 
-    console.log(getPipelineStages)
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-            results: [{
-                objectId: 245,
-                title: `${event.queryStringParameters.dealname}`,
-                link: "http://example.com/1",
-                created: "2016-09-15",
-                priority: "HIGH",
-                project: "API",
-                reported_by: "msmith@hubspot.com",
-                description: "Customer reported that the APIs are just running too fast. This is causing a problem in that they're so happy.",
-                reporter_type: "Account Manager",
-                status: "In Progress",
-                ticket_type: "Bug",
-                updated: "2016-09-28",
-                actions: [{
-                        type: "CONFIRMATION_ACTION_HOOK",
-                        httpMethod: "GET",
-                        uri: "https://hopeful-golick-30e6f5.netlify.com/.netlify/functions/action",
-                        label: "Example action",
-                        associatedObjectProperties: [
-                            "dealname",
-                            "dealstage",
-                            "hs_object_id"
-                        ],
-                        confirmationMessage: "Are you sure you want to run example action?",
-                        confirmButtonText: "Yes",
-                        cancelButtonText: "No"
-                    },
-                    {
-                        type: "ACTION_HOOK",
-                        httpMethod: "GET",
-                        uri: "https://hopeful-golick-30e6f5.netlify.com/.netlify/functions/action",
-                        label: "no prompt action",
-                        associatedObjectProperties: [
-                            "dealname",
-                            "dealstage",
-                            "hs_object_id"
-                        ]
-                    },
-                    {
-                        type: "IFRAME",
-                        width: 890,
-                        height: 748,
-                        uri: "https://example.com/iframe-contents",
-                        label: "Test iFrame",
-                        associatedObjectProperties: []
-                    }
-                ]
-            }],
-            submissionAction: {
-                type: "IFRAME",
-                width: 890,
-                height: 748,
-                uri: "https://bat-staging.trussle.com/application/87c6b07f-97e8-44a4-bf40-19c5e28e7e84",
-                label: "Submission"
-            },
-            editDetailsAction: {
-                type: "IFRAME",
-                width: 890,
-                height: 748,
-                uri: "https://bat-staging.trussle.com/edit/87c6b07f-97e8-44a4-bf40-19c5e28e7e84",
-                label: "Edit Customer Data"
-            }
-        })
-    });
 };
