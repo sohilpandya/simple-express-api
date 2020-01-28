c6503711dcdce2e2c5c60ea1f4605baa24955433
Sohil Pandya <sohilpandya@me.com>
2020-01-28 12:08:09 +0000

    removed api key and rotated the key. :devil:

--- functions/action.js
+++ functions/action.js
@@ -7,7 +7,7 @@ exports.handler = function(event, context, callback) {
 
     const id = event.queryStringParameters.associatedObjectId
     const uri = `https://api.hubapi.com/deals/v1/deal/${id}?hapikey=${process.env.HAPI_KEY}`
-        // need to make a call to https://api.hubapi.com/deals/v1/deal/1292767896?hapikey=61bce155-c17a-49aa-adab-e062ca9c13a3
+
 
     axios.put(uri, {
             body: JSON.stringify({
